4da540abd306d4dd53f9a63091a838782bbc6d1f
MohanaPoojary <mohan.poojary@manipal.net>
2023-12-26 15:55:09 +0530

    commit all

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import About from './components/about/about';
+import Contact from './components/contact/contact';
+import Header from './components/header/header';
+import Nav from './components/nav/nav';
+import Experience from './components/experience/experience';
+import Services from './components/services/services';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from './components/testimonials/testimonials';
+import Footer from './components/footer/footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact />
+      <Footer/>
     </div>
   );
 }
